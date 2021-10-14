cad054e74ec8078b0dd23fe2783ec9e31cfe7f4b
Alessandra D. de Souza <alessandra.souza@metasix.com.br>
2021-10-13 21:53:49 -0300

    Initial - next config, add player component

---
+++ components/players/config.js
@@ -0,0 +1,72 @@
+import { Component } from 'react'
+import { AiOutlineUserAdd } from 'react-icons/ai'
+
+export default class PlayersConfig extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            newPlayer: ''
+        }
+
+        this.buildProfile = this.buildProfile.bind(this);
+        this.newPlayerName = this.newPlayerName.bind(this);
+        this.sortPlayers = this.sortPlayers.bind(this);
+    }
+
+    newPlayerName(e) {
+        this.setState({newPlayer: e.target.value})
+      }
+    
+      buildProfile(e) {
+        e.preventDefault();
+        const props = this.props;
+        const newPlayer = {name: this.state.newPlayer, score: 0, id: props.players.length};
+        props.addPlayer(newPlayer);
+        this.setState({
+          newPlayer: ''
+        })
+      }
+    
+      sortPlayers() {
+        let list = this.props.players;
+        let min = Math.ceil(0);
+        let max = Math.floor(list.length);
+    
+        for(var i = 0; i < list.length; i++) {
+          let rand = Math.floor(Math.random() * (max - min + 1)) + min;
+          if(list[i] && list[rand]) {
+            let temp = list[i];
+            list[i] = list[rand]
+            list[rand] = temp;
+          }
+        }
+    
+        this.setState({
+          players: list
+        })    
+      }
+
+      render() {
+          const list = this.props.players;
+          return (
+              <>
+                <h2>Jogadores &rarr;</h2>
+              
+              <form onSubmit={this.buildProfile}>
+                <input type="text" id="newPlayerName" value={this.state.newPlayer} onChange={this.newPlayerName} />
+                <button type="submit"><AiOutlineUserAdd /></button>
+              </form>
+              <ol>
+                {list.length 
+                  ? list.map(player => <li key={player.id}>{player.name}</li>) 
+                  : <li>No players yet</li>}
+              </ol>
+              
+
+              <button type="button" onClick={this.sortPlayers}>
+                Embaralhar
+              </button>
+              </>
+          )
+      }
+}
--- next.config.js
+++ next.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   reactStrictMode: true,
+  i18n: {
+    locales: ['pt-BR', 'en-US'],
+    defaultLocale: 'pt-BR',
+    localeDetection: true,
+    domains: [
+      {
+        domain: 'example.com',
+        defaultLocale: 'en-US',
+      },
+      {
+        domain: 'example.nl',
+        defaultLocale: 'nl-NL',
+      }
+    ],
+  }
 }
